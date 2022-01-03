d0008cdd5dd48aee87f432e976d6cf4c207ff867
pablinho <p.olmedosaez@gmail.com>
2022-01-02 23:29:18 -0400

    Creamos una funcion que se utiliza cada vez que se detecta un error, además de generar mensajes de error personalizados

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 import { valida } from "./validaciones.js";
-const inputs = document.querySelectorAll("input")
 
-inputs.forEach(input =>{
-    input.addEventListener("blur", ()=>{
-        valida(input.target)
-    })
-})
+const inputs = document.querySelectorAll("input");
+
+inputs.forEach((input) => {
+  input.addEventListener("blur", (input) => {
+    valida(input.target);
+  });
+});
--- js/validaciones.js
+++ js/validaciones.js
@@ -1,39 +1,79 @@
-export function valida(input){
-    const tipoDeInput = input.dataset.tipo;
-    if (validadores[tipoDeInput]) {
-        validadores[tipoDeInput(input)]
-        
+export function valida(input) {
+  const tipoDeInput = input.dataset.tipo;
+  if (validadores[tipoDeInput]) {
+    validadores[tipoDeInput](input);
+  }
+
+  if (input.validity.valid){
+    input.parentElement.classList.remove("input-container--invalid")
+    input.parentElement.querySelector(".input-message-error").innerHTML= ""
+  }else{
+    input.parentElement.classList.add("input-container--invalid")
+    input.parentElement.querySelector(".input-message-error").innerHTML= mostrarMensajeDeError(tipoDeInput, input)
+
+  }
+}
+
+const tipoDeErrores = [
+  "valueMissing",
+  "typeMismatch",
+  "patternMismatch",
+  "customError"
+
+]
+
+
+const mensajesDeError = {
+  nombre:{ //objeto
+    valueMissing: "Este campo no puede estar vacío"
+  },
+  email:{
+    valueMissing: "Este campo no puede estar vacío",
+    typeMismatch: "El correo no es valido"
+  },
+  password: {
+    valueMissing: "Este campo no puede estar vacío",
+    patternMismatch: "Al menos 6 caracteres, máximo 12, debe contener una letra minúscula, una letra mayúscula, un número y no puede contener caracteres especiales."
+
+  },
+  nacimiento: {
+    valueMissing: "Este campo no puede estar vacío",
+    customError: "Debes tener almenos 18 años de edad"
+
+  }
+}
+
+const validadores = {
+  nacimiento: (input) => validarNacimiento(input),
+};
+
+function mostrarMensajeDeError(tipoDeInput, input){
+  let mensaje = ""
+  tipoDeErrores.forEach(error =>{
+    if(input.validity[error]){
+      mensaje = mensajesDeError[tipoDeInput][error]
     }
+  })
+
+  return mensaje
 }
 
-const validadores ={
-    nacimiento: input => validarNacimiento(input)
+function validarNacimiento(input) {
+  const fechaCliente = new Date(input.value);
+  let mensaje = "";
+  if (!mayorDeEdad(fechaCliente)) {
+    mensaje = "Debes tener al menos 18 años de edad";
+  }
+
+  input.setCustomValidity(mensaje);
 }
 
-const inputNacimiento = document.querySelector("#birth")
-inputNacimiento.addEventListener("blur", (evento)=>{ //Blur hace refencia a cuando se quita el foco (puntero), en este caso del input Fecha de nacimiento
-   validarNacimiento(evento.target);
-});
-
-function validarNacimiento(input){
-    const fechaCliente = new Date(input.value);
-    mayorDeEdad(fechaCliente)
-    let mensaje = ""
-    if(!mayorDeEdad(fechaCliente)){
-        mensaje = "Debes tener al menos 18 años de edad"
-    }
-    input.setCustomValidity(mensaje)
-
-}
-
-
-
-function mayorDeEdad(fecha){
-    const fechaActual = new Date();
-    const diferenciaFechas = new Date(
-        fecha.getUTCFullYear()+ 18,
-        fecha.getUTCMonth(),
-        fecha.getUTCDate());
-    return (diferenciaFechas <= fechaActual )
-
+function mayorDeEdad(fecha) {
+  const fechaActual = new Date();
+  const diferenciaFechas = new Date(
+    fecha.getUTCFullYear() + 18,
+    fecha.getUTCMonth(),
+    fecha.getUTCDate()
+  );
+  return diferenciaFechas <= fechaActual;
 }
